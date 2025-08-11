fd568542eae70ccf8c5c0786f547286078c34e88
NICOLASJIMENJEZ <nicolasjimenezguzman1@gmail.com>
2025-08-11 11:05:00 -0500

    Update conexion.php

--- modulos/conexion.php
+++ modulos/conexion.php
@@ -1,16 +1,16 @@
 <?php
-$host     = 'dpg-d24l0l15pdvs73bvvmq0-a';
-$port     = '5432';
-$dbname   = 'life_gym_db';
-$user     = 'life_gym_db_user';
-$password = '0BaR53ptUeZaLHwtIBbMtuZ6cvYtCu3p';
+$host = 'dpg-d24l0l15pdvs73bvvmq0-a.oregon-postgres.render.com';
+$port = 5432;
+$user = 'life_gym_db_user';
+$pass = '0BaR53ptUeZaLHwtIBbMtuZ6cvYtCu3p';
+$db   = 'life_gym_db';
 
 try {
-    $dsn = "pgsql:host=$host;port=$port;dbname=$dbname";
-    $conexion = new PDO($dsn, $user, $password, [
+    $dsn = "pgsql:host=$host;port=$port;dbname=$db;sslmode=require";
+    $conn = new PDO($dsn, $user, $pass, [
         PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
     ]);
-    // echo "✅ Conexión exitosa a PostgreSQL";
+    echo "✅ Conectado a PostgreSQL con éxito";
 } catch (PDOException $e) {
     die("❌ Error de conexión: " . $e->getMessage());
 }
